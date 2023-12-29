b148efb5f910280ba8ae92877a44e97ac7212f4a
mani <manikandan.tms88@gmail.com>
2023-12-29 19:29:41 +0530

    added career layout

---
+++ src/layout/CareerLayout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+function CareerLayout() {
+  return (
+    <div>
+      <h2>Careers</h2>
+      <p>Lorem</p>
+      <Outlet />
+    </div>
+  )
+}
+
+export default CareerLayout
