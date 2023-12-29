c3afe531312a952a65ea0cb0dc1bda730009b54a
mani <manikandan.tms88@gmail.com>
2023-12-29 19:28:49 +0530

    added career layout

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import RootLayout from "./layout/RootLayout"
 import HelpLayout from "./layout/HelpLayout"
 import Faq from "./page/help/Faq"
 import Contact from "./page/help/Contact"
-
+import Careers, { careersLoader} from "./page/career/Careers"
+import CareerLayout from "./layout/CareerLayout"
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />}></Route>
       <Route path="about" element={<About />}></Route>
+
+      {/* Help laypout */}
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />}></Route>
         <Route path="contact" element={<Contact />}></Route>
       </Route>
+
+      {/* Careerlayout */}
+      <Route path="careers" element={<CareerLayout />}>
+        <Route index element={<Careers />} loader={careersLoader}></Route>
+      </Route>
+
+      {/* Error Handling */}
       <Route path="*" element={<NotFound />} />
     </Route>
   )
